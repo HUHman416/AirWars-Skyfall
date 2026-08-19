e57cdb0bb02ee64e4eeb85b47ab7f745b1625ac1
HUHman416 <michaelvandegrift1@gmail.com>
2026-08-18 19:22:53 -0700

    0.2 foundation: wire general Skyfall diagnostics into console commands

--- gamemode/concommands.lua
+++ gamemode/concommands.lua
@@ -1,7 +1,8 @@
-concommand.Add( "set_time", function( ply, cmd, args )
-	if IsValid(ply) and !ply:IsAdmin() then return end
-	AirWars:SetTimeLeft(util.StringToType( args[1], "int" ))
-end )
+concommand.Add("set_time", function(ply, _, args)
+    if IsValid(ply) and not ply:IsAdmin() then return end
+    AirWars:SetTimeLeft(tonumber(args[1]) or 0)
+end)
 
--- AirWars: Skyfall developer test harness.
+-- AirWars: Skyfall developer tooling.
+include("server/dev_admin.lua")
 include("server/dev_tests_v2.lua")
