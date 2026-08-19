b60657d1dcb1e0a6ab5d2aa20624cff12e87f9a5
HUHman416 <michaelvandegrift1@gmail.com>
2026-08-18 19:32:57 -0700

    0.4 fire and steel: add configurable Skyfall weapon entity definition

---
+++ entities/entities/aw_weapon_skyfall/shared.lua
@@ -0,0 +1,11 @@
+ENT.Base = "aw_weapon_base"
+ENT.Type = "anim"
+ENT.Author = "AirWars: Skyfall Contributors"
+ENT.Spawnable = false
+ENT.Useable = true
+ENT.CustomText = "Operate"
+
+ENT.ShootingAngle = Angle()
+ENT.ShootingOffset = 70
+ENT.AmmoAmount = 1
+ENT.Cooldown = 1
